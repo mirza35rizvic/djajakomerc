4eb9eba417604dbca2c5ac797099c124dea4d1a4
mirza35rizvic <mirzarizvic35@gmail.com>
2021-07-01 20:20:00 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -71,24 +71,27 @@ const StyledNav  = styled.nav`
         padding-left: 10rem;
         position: relative;
     }
-    @media(max-width: 1300px){
-       flex-direction: column;
-       padding: 2rem 3rem;
-       #logo {
-           display: inline-block;
-           margin: 1rem
-       }
-       ul{
-           padding: 2rem;
-           justify-content: space-around;
-           width: 100%;
-           li{
-               padding: 0;
-           }
-       }
-      
-    }
+   
+       
+    @media (max-width: 1300px) {
+        flex-direction: column;
+        padding: 2rem 1rem;
+        #logo {
+            display: inline-block;
+            margin: 1rem;
+        }
+        ul {
+            padding: 2rem;
+            justify-content: space-around;
+            width: 100%;
+            li {
+                padding: 0;
+            }
+        }
+    }   
 `;
+
+
 const Line = styled(motion.div)`
     height: 0.3rem;
     background: #23d997;
@@ -96,11 +99,11 @@ const Line = styled(motion.div)`
     position: absolute;
     bottom: -80%;
     left: 60%;
-    @media(max-bottom: 1300px) {
-        left: 0;
-        width: 50%;
-        }
-        
+    @media(max-width: 1300px){
+        left: 0%;
+        width: 100%;
+    }
+       
     
   
 `;
