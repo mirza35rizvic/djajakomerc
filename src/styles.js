7ad1483ee876e2ff397db925570f6482d70fd95e
mirza35rizvic <mirzarizvic35@gmail.com>
2021-07-02 11:04:58 +0200

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,37 +1,33 @@
+
 import styled from "styled-components";
 import {motion} from 'framer-motion';
 
 //Styled Components
 export const About = styled(motion.div)`
-    min-height: 120vh;
+    min-height: 90vh;
     display: flex;
     align-items: center;
     justify-content: center;
     padding: 5rem 10rem;
     color: white;
     
-    @media(max-width: 750px){
+    @media(max-width: 1300px){
         display: block;
         padding: 2rem 2rem;
         text-align: center;
-
-         min-height: 70vh;
-         margin-top: 5rem
-        
 }
     
 `;
 
 export const Description = styled.div`
     flex: 1;
-    
+    padding-right: 0;
     h2{
         font-weight: lighter;
     }
     @media (max-width: 1300px){
         padding: 1rem 1rem;
-        margin-bottom: 2rem; 
-        margin-top: 9rem;
+        margin-bottom: 20rem; 
         margin-left: 1rem;
         margin-right: 2rem;
         h2{
@@ -60,8 +56,8 @@ export const Image = styled.div`
         img{
             width: 50%;
             height: 80%;
-            margin-top: 10rem;
-            margin-bottom: 8rem;
+            margin-top: 1rem;
+            margin-bottom: 15rem;
         }
       
 }
@@ -69,3 +65,4 @@ export const Image = styled.div`
 
 export const Hide = styled.div`
     overflow: hidden;
+`;
